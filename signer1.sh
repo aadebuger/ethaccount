920450c24641a5916794bf4ecab4798e4cd1d5a9
aadebuger <zhvxxh@gmail.com>
2018-08-20 16:45:57 +0800

    env

--- signer1.sh
+++ signer1.sh
@@ -1,1 +1,2 @@
-geth --datadir ./data --networkid 55661 --port 2002 --unlock a4a6340dd805cda6d08a44d31ac5971b973f2656 console
+export s1account=4a6340dd805cda6d08a44d31ac5971b973f2656
+geth --datadir ./data --networkid 55661 --port 2002 --unlock $s1account console
