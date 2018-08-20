ff366494b8b2588abc60901be62108b3d8cff1d7
aadebuger <zhvxxh@gmail.com>
2018-08-20 16:46:10 +0800

    env

--- signer2.sh
+++ signer2.sh
@@ -1,1 +1,2 @@
-geth --datadir ./data --networkid 55661 --port 2003 --unlock 5bab733939284bf58bbee5bbeb97911f7b74ac74  console
+export s2account=
+geth --datadir ./data --networkid 55661 --port 2003 --unlock $s2account  console
